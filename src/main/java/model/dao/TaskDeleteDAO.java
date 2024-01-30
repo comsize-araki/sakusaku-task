2c761e3043e141f07804cc460c24334b20a7b125
Tir812 <souju03233@icloud.com>
2024-01-30 17:51:19 +0900

    javadocコメントの追加

--- src/main/java/model/dao/TaskDeleteDAO.java
+++ src/main/java/model/dao/TaskDeleteDAO.java
@@ -4,7 +4,19 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+
+/**
+ * 取得したtask_idのレコードをt_taskテーブルから消す
+ * @author taira
+ */
 public class TaskDeleteDAO {
+	/**
+	 * 取得したtask_idのレコードをt_taskテーブルから消す
+	 * @param taskid
+	 * @return 削除した件数
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
 	public int TaskDelete(int taskid) throws SQLException,ClassNotFoundException{
 		String sql = "DELETE FROM t_task WHERE task_id = ?";
 		
@@ -18,6 +30,11 @@ public class TaskDeleteDAO {
 		return count;
 	}
 }
+	
+	/**
+	 * 取得したtask_idのレコードをt_commentテーブルから消す
+	 * @author taira
+	 */
 	public int DeleteSuccess(int taskid) throws SQLException,ClassNotFoundException{
 		String sql = "DELETE FROM t_comment WHERE task_id = ?";
 		
--- src/main/java/model/dao/TaskListDAO.java
+++ src/main/java/model/dao/TaskListDAO.java
@@ -10,7 +10,17 @@ import java.util.List;
 
 import model.entity.TaskBean;
 
+/**
+ * タスクの一覧表示するためのsql文を実行してその結果をlistに入れてTaskbean型で返す
+ * @author taira
+ */
 public class TaskListDAO {
+	/**
+	 *一覧表示
+	 * @return List<TaskBean>
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
 	public List<TaskBean> ListView() throws SQLException,ClassNotFoundException{
 		//一覧表示するためのsql文
 		String sql="SELECT t1.task_id,t1.task_name,t2.category_name,t1.limit_date, t3.user_name,t4.status_name,t1.memo FROM t_task t1 INNER JOIN m_category t2 ON t1.category_id = t2.category_id INNER JOIN m_user t3 ON t1.user_id = t3.user_id INNER JOIN m_status t4 ON t1.status_code = t4.status_code;";
