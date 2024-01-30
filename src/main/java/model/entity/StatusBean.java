caae854daa368e429605fea0c39f2902e089e850
com-Mo42 <kenta.mori@comsize.com>
2024-01-30 17:07:03 +0900

    JavaDocコメントの追加

--- src/main/java/model/entity/StatusBean.java
+++ src/main/java/model/entity/StatusBean.java
@@ -1,29 +1,47 @@
 package model.entity;
 
-//ステータス情報を保持するエンティティ
+/**
+ * ステータス情報を保持するエンティティ
+ * @author mori
+ */
 public class StatusBean {
 
-	//ステータスコード
+	/**
+	 * ステータスコード
+	 */
 	private String statusCode;
-	
-	//ステータス名
+
+	/**
+	 * ステータス名
+	 */
 	private String statusName;
 
-	//アクセサ
+	/**
+	 * @return statusCode
+	 */
 	public String getStatusCode() {
 		return statusCode;
 	}
 
+	/**
+	 * @param statusCode セットする statusCode
+	 */
 	public void setStatusCode(String statusCode) {
 		this.statusCode = statusCode;
 	}
 
+	/**
+	 * @return statusName
+	 */
 	public String getStatusName() {
 		return statusName;
 	}
 
+	/**
+	 * @param statusName セットする statusName
+	 */
 	public void setStatusName(String statusName) {
 		this.statusName = statusName;
 	}
-	
+
 }
